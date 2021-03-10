c083c1962895bd8dff31183b86957a8187e59fda
Carlos <carmaujica2@gmail.com>
2021-03-10 11:22:15 -0500

    checkpassword function

---
+++ src/checkPassword.java
@@ -0,0 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class checkPassword {
+  // checks if a pass is valid
+  // valid pass have at least 1 number, 1 lowercase char, 1 uppercase char
+  // a special symbol and a length of {8,20}
+  boolean checkPassword(String pass) {
+    Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$");
+    return pattern.matcher(pass).find();
+  }
+}
